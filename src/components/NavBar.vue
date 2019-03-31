7f35c368bfdcb87e611bb11af7b584d686ddc69f
Vong3432 <vongnyuksoon2000@gmail.com>
2019-03-31 21:36:06 +0800

    pb

---
+++ src/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav>
+        <a 
+        v-for="(item,index) in navItems"
+        :key="index" 
+        :href="item.src"
+        >
+        {{ item.name }}
+        </a>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            navItems:[
+            {
+                name:'Home',
+                src:'#',
+                isActive:true
+            },
+            {
+                name:'About me',
+                src:'#',
+                isActive:false
+            },
+            {
+                name:'My portfolio',
+                src:'#',
+                isActive:false
+            },
+            ]
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+.navbar
+{
+
+}
+
+</style>
